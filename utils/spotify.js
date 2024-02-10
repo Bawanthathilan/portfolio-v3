5d05fa28ba9c827127982d9357a76fe2bcd38808
Bawanthathilan <bawantharathnayaka@gmail.com>
2024-02-10 18:25:46 +0530

    fixed env file issue

--- utils/spotify.js
+++ utils/spotify.js
@@ -1,16 +1,16 @@
 import querystring from 'querystring';
+import {
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  SPOTIFY_REFRESH_TOKEN
+} from '@/config/keys';
 
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
-// const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-// const client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
-// const refresh_token = process.env.REACT_APP_SPOTIFY_REFRESH_TOKEN;
-
-const client_id = '4c65faa9a95c44f3aeebddba4a2ea2ce';
-const client_secret = '258d5add6d6645ffaf6bc8ef5878aa98';
-const refresh_token =
-  'AQBHABFiUGq9q0DY7ENaFHqAOnoOwEXcmKHJL0sAmctVltXUZmvdcTevWbf9185twBHHTSEpZ3uzVc1cF1l9hFi4W3nSUiKSmpmpJ8tcDv1fdurdxxdsqHJUsm2WOu-nXGE';
+const client_id = SPOTIFY_CLIENT_ID;
+const client_secret = SPOTIFY_CLIENT_SECRET;
+const refresh_token = SPOTIFY_REFRESH_TOKEN;
 
 const getAccessToken = async () => {
   const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
